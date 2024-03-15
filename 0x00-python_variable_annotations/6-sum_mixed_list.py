50ddfe633ebb5507ad0b5f5e46620ce08cd78c6d
RedaMjibel <reda_mjibel@hotmail.com>
2024-03-14 19:54:57 -0700

    test mxd

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Complex types - mixed list
+"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lists: List[Union[int, float]]) -> float:
+    """ Returns the sum as floats """
+    return sum(mxd_list)
