14c09d0138d0a1ab9b5d9507ced7a420f82e1133
RedaMjibel <reda_mjibel@hotmail.com>
2024-03-15 17:51:27 -0700

    union

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,6 +6,6 @@ A type annotated function
 from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple containing k and the square of v."""
-    return k, float(v) ** 2
+    return (k, v**2)
