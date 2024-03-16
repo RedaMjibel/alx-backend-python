0a536ff3be84212f19fb6bab316ae10a964655b1
RedaMjibel <reda_mjibel@hotmail.com>
2024-03-15 17:48:00 -0700

    update

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+"""
+A type annotated function
+"""
+
+
 def to_kv(k: str, v: int | float) -> Tuple[str, float]:
     """Return a tuple containing k and the square of v."""
     return k, float(v) ** 2
