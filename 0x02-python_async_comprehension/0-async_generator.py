35fd70f841cb7b38fc5bab085cd36ece3e0c024d
RedaMjibel <reda_mjibel@hotmail.com>
2024-03-19 16:03:40 -0700

    test

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" 
+create an async function
+"""
+
+import asyncio
+import random
+
+async def async_generator():
+    """ generates a random number """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
