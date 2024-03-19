48e09eaa90d30e435f092103f322c0991eec7e91
RedaMjibel <reda_mjibel@hotmail.com>
2024-03-19 16:05:48 -0700

    update an pycodestyle

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-""" 
+"""
 create an async function
 """
 
 import asyncio
 import random
 
-async def async_generator():
+
+async def async_generator(): -> Generator[float, None, None]:
     """ generates a random number """
     for i in range(10):
         await asyncio.sleep(1)
