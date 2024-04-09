25b2f01d37380070d2f50da2187958f4fde353c9
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-09 02:48:33 +0000

    test memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import unittest
-from unittest.mock import patch
 from parameterized import parameterized
 from utils import (access_nested_map, get_json, memoize)
+from unittest.mock import patch
+
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -25,19 +26,42 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 			
 class TestGetJson(unittest.TestCase):
-    """ Class for Testing Get_json """
+    """ Class for Testing Get Json """
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
     def test_get_json(self, test_url, test_payload):
+        """ Test for the utils.get_json function to check
+        that it returns the expected result."""
         config = {'return_value.json.return_value': test_payload}
         patcher = patch('requests.get', **config)
         mock = patcher.start()
         self.assertEqual(get_json(test_url), test_payload)
         mock.assert_called_once()
         patcher.stop()
+
+class TestMemoize(unittest.TestCase):
+    """ test class to tes utils.memoize"""
+
+    def test_memoize(self):
+
+        class TestClass:
+            """ Test Class for wrapping with memoize """
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
 
 if __name__ == "__main__":
     unittest.main()
