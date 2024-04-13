441a8bc19618ddbf83072e7ee88ff3d4f62ad449
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-13 00:34:24 +0000

    updat

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -13,8 +13,8 @@ class TestGithubOrgClient(unittest.TestCase):
     """Test cases for GithubOrgClient"""
 
     @parameterized.expand([
-        ('google',),
-        ('abc',)
+        ('google'),
+        ('abc')
     ])
     @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
