e26e2e016ed39b26bf495f5d37f8729fffdc5b8c
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-13 00:58:23 +0000

    testurl

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -28,3 +28,17 @@ class TestGithubOrgClient(unittest.TestCase):
         test_response = org_test.org
         self.assertEqual(test_response, mock_org.return_value)
         mock_org.assert_called_once()
+
+    @patch('client.GithubOrgClient.org')
+    def test_public_repos_url(self, mock_org):
+        """ Tests repos_url and compares them to mock_org """
+        org_payload = {"repos_url": "https://api.github.com/orgs/test/repos"}
+
+        mock_org.return_value = org_payload
+
+        client = GithubOrgClient("test")
+
+        public_repos_url = client._public_repos_url
+
+        self.assertEqual(public_repos_url, org_payload["repos_url"])
+
